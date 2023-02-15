0a2d37a5bc76900c4947d3f931bc074e1750a4e3
Larry.Lai <Larry.Lai@portwell.com.tw>
2023-02-15 09:26:17 +0800

    init

---
+++ api.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+
+namespace LineNotify_OpenAPI.Client
+{
+    using LineNotify.Interface.Data;
+    using LineNotify.Interface.Func;
+
+    public class api : Func
+        , Iapi<Result>
+    {
+        public Result notify(string token, string message)
+        {
+            RestRequest request = new RestRequest("api/notify", Method.Post);
+            request.AddQueryParameter("token", token);
+            request.AddQueryParameter("message", message);
+
+            return ExecuteJsonConvert<Result>(request);
+        }
+    }
+}
