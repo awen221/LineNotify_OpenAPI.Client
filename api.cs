4193598646181d60a24c7ff7ddd231d737eaa161
Larry.Lai <Larry.Lai@portwell.com.tw>
2023-02-15 11:28:37 +0800

    移除不必要耦合

--- api.cs
+++ api.cs
@@ -1,20 +1,28 @@
 ﻿using RestSharp;
 
+using LineNotify.Interface.Data;
+using OpenAPI.Client;
+using Newtonsoft.Json;
+
 namespace LineNotify_OpenAPI.Client
 {
-    using LineNotify.Interface.Data;
-    using LineNotify.Interface.Func;
-
-    public class api : Func
-        , Iapi<Result>
+    public class api : ClientBase
     {
-        public Result notify(string token, string message)
+        public Result notify(string api_url, string token, string message)
         {
-            RestRequest request = new RestRequest("api/notify", Method.Post);
+            var request = new RestRequest("api/notify", Method.Post);
             request.AddQueryParameter("token", token);
             request.AddQueryParameter("message", message);
 
-            return ExecuteJsonConvert<Result>(request);
+            return ExecuteJsonConvert(api_url, request);
+        }
+
+        Result ExecuteJsonConvert(string api_url, RestRequest request)
+        {
+            var json = Execute(api_url, request);
+            var result = JsonConvert.DeserializeObject<Result>(json);
+
+            return result;
         }
     }
 }
